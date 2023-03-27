4d0a374c227d1eaf1f458109ba28967ad360c110
vinicius webster <viniciuswebster99@gmail.com>
2023-03-27 20:49:49 -0300

    projeto numero secreto

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,26 @@
+const elementoChute = document.getElementById('chute')
+
+const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br'
+recognition.start()
+
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e) {
+   chute = e.results[0][0].transcript
+   exibeChuteNaTela(chute);
+   verificaValorValido(chute);
+}
+
+function exibeChuteNaTela(chute) {
+    elementoChute.innerHTML = `
+    <div> Você disse </div>
+    <span class="box">${chute}</span>`
+}
+
+
+recognition.addEventListener('end', ()=>{
+    recognition.start()
+})
---
+++ app/validacao.js
@@ -0,0 +1,44 @@
+function verificaValorValido(chute) {
+    const numero = +chute;
+
+    if(chuteÉInvalido(numero)){
+        elementoChute.innerHTML += '<div> Valor inválido</div>'
+        console.log('valor inválido')
+        return
+    }
+
+    if(numeroNãoPermitido(numero)){
+        elementoChute.innerHTML +=` <div> Valor inválido: Fale um número entre ${menorValor} e ${maiorValor} </div>`;
+        return
+    }
+
+    if(numero === numeroSecreto){
+        document.body.innerHTML = `
+        <h2> Você acertou!</h2>
+        <h3> O número secreto era ${numeroSecreto}</h3>
+        <button id="jogar-novamente" class="btn-jogar"> Jogar Novamente</button>`;
+        
+    } else if(numero > numeroSecreto){
+        elementoChute.innerHTML += 
+        `<div>O número secreto é menor <i class="fa-solid fa-angle-down"></i></div>`
+    } else {
+        elementoChute.innerHTML += 
+        `<div>O número secreto é maior <i class="fa-solid fa-angle-up"></i></div>`
+    }
+
+}
+
+function numeroNãoPermitido(numero) {
+    return numero > maiorValor || numero < menorValor
+}
+
+function chuteÉInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+
+document.body.addEventListener('click', e =>{
+    if(e.target.id == 'jogar-novamente'){
+        window.location.reload()
+    }
+})
